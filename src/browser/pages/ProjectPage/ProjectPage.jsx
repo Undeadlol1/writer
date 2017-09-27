647d3089c9a6fe178f2a1acbe6994a995626e359
Mihail Paley <undeadlol1@mail.ru>
2017-09-27 21:04:28 +0500

    (ProjectPage) add image

--- src/browser/pages/ProjectPage/ProjectPage.jsx
+++ src/browser/pages/ProjectPage/ProjectPage.jsx
@@ -14,13 +14,23 @@ class ProjectPage extends PureComponent {
 		const { project, loading } = this.props
 		// TODO plot point scenes only
 		const scenes = project.get('Scenes')
+		const image = project.get('image')
 		return 	<PageWrapper
 					loading={loading}
 					className='ProjectPage'
 				>
 					<Grid fluid>
-						<center><h1>{project.get('title')}</h1></center>
-						<center><h2>{project.get('shortPitch')}</h2></center>
+						<Row>
+							<Col xs={12}>
+								<center><h1>{project.get('title')}</h1></center>
+							</Col>
+							<Col xs={12}>
+								<center>
+									<img src={image} height="300" alt="" />
+									<h2>{project.get('shortPitch')}</h2>
+								</center>
+							</Col>
+						</Row>
 						<ProgressStepper />
 						{
 							project.get('progress') >= 3 && scenes.map(scene => <p key={scene.get('id')}>{scene.get('description')}</p>)
